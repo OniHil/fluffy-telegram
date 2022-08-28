f9a8e6bb17770152f6712df6915c696cafec455d
Mikael Kemstedt <micke.k4@gmail.com>
2022-08-28 15:23:23 +0200

    Map movement workds but it flickers when I click

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,7 @@ fn main() {
      *
      * Resources represent state, or globally unique data
      *
-     * Get entities by Query<&Component, With<Component>
+     * Get entities by Query<(Entity, &Resource?), With<Component>
      * and get values of resources by referencing Res for reading and ResMut for writing
      */
     App::new()
@@ -23,13 +23,13 @@ fn main() {
             mode: WindowMode::BorderlessFullscreen,
             ..default()
         })
-        .insert_resource(WinitSettings::desktop_app())
-        .insert_resource(CursorState::default())
         .add_startup_system(setup)
         .add_system_to_stage(CoreStage::PreUpdate, change_cursor_state)
-        .add_system_to_stage(CoreStage::Update, prepare_movement)
-        .add_system_to_stage(CoreStage::PostUpdate, sync_cursor_and_moving)
+        .add_system_to_stage(CoreStage::Update, transform_cursor)
+        .add_system_to_stage(CoreStage::Update, mark_moving_status)
+        //.add_system_to_stage(CoreStage::PostUpdate, sync_cursor_and_moving)
         .add_system_to_stage(CoreStage::PostUpdate, moving)
+        .add_system_to_stage(CoreStage::PostUpdate, still)
         .add_plugins(DefaultPlugins)
         .run();
 }
@@ -56,6 +56,7 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     commands
         .spawn()
         .insert_bundle((Transform::default(), GlobalTransform::default(), Cursor));
+    commands.insert_resource(CursorState::default());
     commands
         .spawn_bundle(SpriteBundle {
             texture: asset_server.load("images/map.png"),
@@ -91,20 +92,29 @@ fn change_cursor_state(
         let window = windows.get_primary().unwrap();
         let cam_transform = q_camera.iter().last().unwrap();
 
-        cursor_state.position = mouse_motion.position // cursor_to_screen(window, cam_transform, mouse_motion.position);
+        cursor_state.position = cursor_to_screen(window, cam_transform, mouse_motion.position);
     }
 }
 
-/*
 fn cursor_to_screen(window: &Window, cam_transform: &Transform, cursor_pos: Vec2) -> Vec2 {
     let window_size = Vec2::new(window.width() as f32, window.height() as f32);
     let screen_pos = cursor_pos - window_size / 2.;
     let out = cam_transform.compute_matrix() * screen_pos.extend(0.0).extend(1.0);
 
     Vec2::new(out.x, out.y)
-}*/
+}
 
-fn prepare_movement(
+fn transform_cursor(
+    cursor_state: ResMut<CursorState>,
+    mut q_cursor: Query<&mut Transform, With<Cursor>>,
+) {
+    for mut transform in q_cursor.iter_mut() {
+        transform.translation.x = cursor_state.position.x;
+        transform.translation.y = cursor_state.position.y;
+    }
+}
+
+fn mark_moving_status(
     mut commands: Commands,
     cursor_state: Res<CursorState>,
     q_movable: Query<Entity, With<Movable>>,
@@ -125,22 +135,11 @@ fn prepare_movement(
     }
 }
 
-fn sync_cursor_and_moving(
+fn still(
     mut commands: Commands,
-    q_moving: Query<(Entity), Added<Moving>>,
-    q_still: Query<(Entity, &Parent), (Added<Still>, With<Parent>)>,
-    q_cursor: Query<Entity, With<Cursor>>,
+    mut q_still: Query<(Entity, &Parent), (Added<Still>, With<Parent>)>,
 ) {
-    // Adds or removes a component that marks an entity for moving.
-    if let Some(movable_entity) = q_moving.iter().next() {
-        if let Some(cursor_entity) = q_cursor.iter().next() {
-            println!("Added cursor parent");
-            commands.entity(cursor_entity).add_child(movable_entity);
-        }
-    }
-    // if it isn't remove the "Moving" component from the entity
-    if let Some((entity, parent)) = q_still.iter().next() {
-        println!("Removed cursor parent");
+    for (entity, parent) in q_still.iter_mut() {
         commands.entity(entity).remove::<Still>();
         commands.entity(**parent).remove_children(&[entity]);
     }
@@ -148,17 +147,15 @@ fn sync_cursor_and_moving(
 
 fn moving(
     mut commands: Commands,
-    mut q_moving: Query<(Entity, &mut Transform, &GlobalTransform), With<Moving>>,
+    mut q_moving: Query<(Entity, &mut Transform, &GlobalTransform), Added<Moving>>,
     q_cursor: Query<(Entity, &GlobalTransform), With<Cursor>>,
 ) {
-    // Get the cursor and the movable entities, assign the cursor as the parent of the movable entities
-    if let Some((cursor_e, cursor_transform)) = q_cursor.iter().next() {
-        for (entity, mut transform, global_transform) in q_moving.iter_mut() {
-            // println!("{:?}", transform);
+    if let Some((cursor_entity, cursor_transform)) = q_cursor.iter().next() {
+        for (moving_entity, mut transform, global_transform) in q_moving.iter_mut() {
             let global_pos = global_transform.translation() - cursor_transform.translation();
-            // println!("{:?}", global_pos);
 
-            //
+            commands.entity(cursor_entity).add_child(moving_entity);
+
             transform.translation.x = global_pos.x;
             transform.translation.y = global_pos.y;
         }
