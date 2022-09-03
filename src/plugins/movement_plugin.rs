d41a0b838bc7bd31b173acc71be1ffdf5a3b1e67
Mikael Kemstedt <micke.k4@gmail.com>
2022-09-03 14:52:32 +0200

    Moved out the zooming to movement_plugin.rs, moved the map sprite to map_plugin.rs

--- src/plugins/movement_plugin.rs
+++ src/plugins/movement_plugin.rs
@@ -1,5 +1,6 @@
-use bevy::{ecs::system::Commands, prelude::*};
-
+use bevy::{ecs::system::Commands, input::mouse::MouseWheel, prelude::*};
+/// Everything conserning movement that is controlled by the mouse should be inside this file.
+/// Everything that uses this plugin will zoom and move at the same rate and in sync.
 pub struct MovementPlugin;
 
 impl Plugin for MovementPlugin {
@@ -9,13 +10,18 @@ impl Plugin for MovementPlugin {
             .add_system(transform_cursor)
             .add_system(mark_moving_status)
             .add_system(moving)
-            .add_system(still);
+            .add_system(still)
+            .add_system(zooming);
     }
 }
 
 // Components
+/// If you want something to be zoomable, then insert this component.
 #[derive(Component)]
-struct Movable;
+pub struct Zoomable;
+/// If it's movable (with the mouse) then add this one.
+#[derive(Component)]
+pub struct Movable;
 #[derive(Component)]
 struct Moving;
 #[derive(Component)]
@@ -30,28 +36,14 @@ struct CursorState {
     holding: bool,
 }
 
-fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+fn setup(mut commands: Commands) {
     commands
         .spawn()
         .insert_bundle((Transform::default(), GlobalTransform::default(), Cursor));
     commands.insert_resource(CursorState::default());
-    commands
-        .spawn_bundle(SpriteBundle {
-            texture: asset_server.load("map/map.png"),
-            transform: Transform {
-                scale: Vec3::splat(0.5),
-                ..default()
-            },
-            sprite: Sprite {
-                flip_x: false,
-                flip_y: false,
-                ..default()
-            },
-            ..default()
-        })
-        .insert(Movable);
 }
 
+/// Updates the cursor state
 fn change_cursor_state(
     mut e_mouse_motion: EventReader<CursorMoved>,
     i_mouse_button: Res<Input<MouseButton>>,
@@ -59,7 +51,6 @@ fn change_cursor_state(
     windows: Res<Windows>,
     q_camera: Query<&Transform, With<Camera>>,
 ) {
-    // Makes sure that the CursorState resource is up-to-date.
     if i_mouse_button.just_pressed(MouseButton::Left) {
         cursor_state.holding = true;
     } else if i_mouse_button.just_released(MouseButton::Left) {
@@ -74,6 +65,7 @@ fn change_cursor_state(
     }
 }
 
+/// Relative position of the cursor, (0, 0) is the middle
 fn cursor_to_screen(window: &Window, cam_transform: &Transform, cursor_pos: Vec2) -> Vec2 {
     let window_size = Vec2::new(window.width() as f32, window.height() as f32);
     let screen_pos = cursor_pos - window_size / 2.;
@@ -82,6 +74,7 @@ fn cursor_to_screen(window: &Window, cam_transform: &Transform, cursor_pos: Vec2
     Vec2::new(out.x, out.y)
 }
 
+/// The Cursor component needs to be in sync with the cursor.
 fn transform_cursor(
     cursor_state: ResMut<CursorState>,
     mut q_cursor: Query<&mut Transform, With<Cursor>>,
@@ -92,27 +85,26 @@ fn transform_cursor(
     }
 }
 
+/// We mark the entities that are going to be moving or were just dropped
 fn mark_moving_status(
     mut commands: Commands,
     cursor_state: Res<CursorState>,
     q_movable: Query<Entity, With<Movable>>,
     q_moving: Query<Entity, With<Moving>>,
 ) {
-    // Adds or removes a component that marks an entity for moving.
     if cursor_state.holding {
-        // If you're holding then add the "Moving" component to the entity
-        // because it's moving we add the cursor as a parent so we can sync thier movement
-        if let Some(movable_entity) = q_movable.iter().next() {
+        for movable_entity in q_movable.iter() {
             commands.entity(movable_entity).insert(Moving);
         }
     } else {
-        if let Some(movable_entity) = q_moving.iter().next() {
+        for movable_entity in q_moving.iter() {
             commands.entity(movable_entity).remove::<Moving>();
             commands.entity(movable_entity).insert(Still);
         }
     }
 }
 
+/// If something is moving, sync it with the cursor (as a parent)
 fn moving(
     mut commands: Commands,
     mut q_moving: Query<(Entity, &mut Transform, &GlobalTransform), Added<Moving>>,
@@ -130,6 +122,7 @@ fn moving(
     }
 }
 
+/// If we dropped it, then since we want them to be still, remove cursor as a parent and mark it as still
 fn still(
     mut commands: Commands,
     mut q_still: Query<(Entity, &Parent), (Added<Still>, With<Parent>)>,
@@ -137,5 +130,19 @@ fn still(
     for (entity, parent) in q_still.iter_mut() {
         commands.entity(entity).remove::<Still>();
         commands.entity(**parent).remove_children(&[entity]);
+    }
+}
+
+/// If we want to zoom in or out we do that here
+/// TODO: Find a better way to zoom, do it logrithimcally / exponentially maybe?
+fn zooming(
+    mut mouse_wheel_events: EventReader<MouseWheel>,
+    mut query: Query<&mut Transform, With<Zoomable>>,
+) {
+    for mut map in &mut query {
+        for scroll in mouse_wheel_events.iter() {
+            map.scale = Vec3::splat(f32::max(scroll.y as f32 + map.scale.max_element(), 0.5));
+            println!("{:?}", map)
+        }
     }
 }
