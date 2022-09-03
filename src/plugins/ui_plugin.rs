ce4ee128d46cc64519f3b7ab5fb3a8528f0b2814
Mikael Kemstedt <micke.k4@gmail.com>
2022-09-03 17:56:52 +0200

    changed hamburger to menu

--- src/plugins/ui_plugin.rs
+++ src/plugins/ui_plugin.rs
@@ -5,13 +5,13 @@ pub struct UIPlugin;
 impl Plugin for UIPlugin {
     fn build(&self, app: &mut App) {
         app.add_startup_system(setup)
-            .add_system(mark_hamburger_expanded)
-            .add_system(expand_retract_hamburger);
+            .add_system(mark_menu_expand)
+            .add_system(expand_retract_menu);
     }
 }
 
 fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
-    commands.insert_resource(HamburgerState::default());
+    commands.insert_resource(MenuState::default());
     commands
         .spawn_bundle(NodeBundle {
             style: Style {
@@ -45,50 +45,50 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
                             position_type: PositionType::Absolute,
                             ..default()
                         },
-                        image: UiImage(asset_server.load("menu/hamburger.png")),
+                        image: UiImage(asset_server.load("menu/right_arrow.png")),
                         ..default()
                     });
                 });
         })
-        .insert(HamburgerMenu);
+        .insert(Menu);
 }
 
 #[derive(Component)]
-struct HamburgerMenu;
+struct Menu;
 
 #[derive(Default)]
-struct HamburgerState {
+struct MenuState {
     expanded: bool,
 }
 
-fn mark_hamburger_expanded(
-    mut q_hamburger: Query<&Interaction, (Changed<Interaction>, With<HamburgerMenu>)>,
-    mut hamburger_state: ResMut<HamburgerState>,
+fn mark_menu_expand(
+    mut q_menu: Query<&Interaction, (Changed<Interaction>, With<Menu>)>,
+    mut menu_state: ResMut<MenuState>,
 ) {
-    for interaction in &mut q_hamburger {
+    for interaction in &mut q_menu {
         match *interaction {
-            Interaction::Clicked => hamburger_state.expanded = !hamburger_state.expanded,
+            Interaction::Clicked => menu_state.expanded = !menu_state.expanded,
             Interaction::Hovered => {}
             Interaction::None => {}
         }
     }
 }
 
-fn expand_retract_hamburger(
-    mut q_hamburger: Query<&mut Style, With<HamburgerMenu>>,
+fn expand_retract_menu(
+    mut q_menu: Query<&mut Style, With<Menu>>,
     //mut windows: ResMut<Windows>,
-    hamburger_state: Res<HamburgerState>,
+    menu_state: Res<MenuState>,
 ) {
-    //let window = windows.get_primary_mut().unwrap();
-    for style in q_hamburger.iter_mut() {
-        if hamburger_state.expanded {
+    let window = windows.get_primary_mut().unwrap();
+    for style in q_menu.iter_mut() {
+        if menu_state.expanded {
             println!("{:?}", style.size);
-            // style.size.width = Val::Px(style.size.width - window.width() * 0.01);
-            // style.size.height = Val::Px(style.size.height - window.height() * 0.01);
+            style.size.width = Val::Px(style.size.width - window.width() * 0.01);
+            style.size.height = Val::Px(style.size.height - window.height() * 0.01);
         } else {
             println!("{:?}", style.size);
-            // style.size.width.add_assign(style.size.width);
-            // style.size.height.add_assign(style.size.add_assign(0.01));
+            style.size.width.add_assign(style.size.width);
+            style.size.height.add_assign(style.size.add_assign(0.01));
         }
     }
 }
